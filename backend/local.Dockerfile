01537ff3de019e086f6e3909e6ca581e31e63b89
ArberHyseni <arber.hyseni@freshfx.at>
2023-10-28 14:02:23 +0200

    initial commit

--- backend/local.Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:10.5.0
-
-WORKDIR /usr/src/app
-
-RUN npm install -g nodemon
-
-RUN npm install -g typescript
-
-EXPOSE 4040 5550
--- backend/src/interfaces/vendors/INext.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Defines Custom method types over Express's NextFunction
- *
- * 
- */
-
-import { NextFunction } from 'express';
-
-export interface INext extends NextFunction {}
