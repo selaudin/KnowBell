25ec6d68004341e87e4b921bad8bd6cea661bb67
Selaudin <a.selaudin@gmail.com>
2023-10-28 17:26:27 +0200

    Fix search box width

--- frontend/src/pages/dashboard/dashboard.component.jsx
+++ frontend/src/pages/dashboard/dashboard.component.jsx
@@ -16,6 +16,7 @@ export function Dashboard() {
             <div style={{marginTop: "1%"}}>
                 <input
                     placeholder={"Search"}
+                    id="search-bar"
                 />
             </div>
 
